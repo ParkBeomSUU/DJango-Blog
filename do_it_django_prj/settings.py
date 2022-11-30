fb085e29a66883db69fea18d6b6f5b96954079f4
BeomSu Park <pps9506@naver.com>
2022-11-30 14:25:50 +0900

    포스트 상세 페이지에 대표 이미지 출력하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h#5$v7b_&+@7^4^wb#t%p=dur8umz*_im&27hi$dgt575r-%z4'
+SECRET_KEY = 'django-insecure-w1y)3=8m#%3u&aq8)7+=se@@g!p2g295u%rj+3*wb8qp(czykp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -111,6 +112,7 @@ TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
+# USE_TZ = True
 USE_TZ = False
 
 
@@ -118,6 +120,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
