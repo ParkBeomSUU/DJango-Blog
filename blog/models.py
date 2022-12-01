cc062f4426a93088720d63bd7ecd9e52f15877da
BeomSu Park <pps9506@naver.com>
2022-12-01 14:25:11 +0900

    네비게이션 바와 푸터 모듈화하기

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 import os.path
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title =models.CharField(max_length=30)
@@ -12,9 +13,11 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    author =models.ForeignKey(User, on_delete=models.CASCADE)
 # author: 추후 작성 예정
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
