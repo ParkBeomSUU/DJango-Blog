f6c276ab1503971546ae3e753e6ed530491cc03c
BeomSu Park <pps9506@naver.com>
2022-12-01 15:24:44 +0900

    포스트 목록, 상세 페이지에서 작성자 정보 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
+from django.contrib.auth.models import User
+
 from .models import Post
 
 
@@ -9,6 +11,8 @@ from .models import Post
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_trump = User.objects.create_user(username='trump', password='somepassword')
+        self.user_obama = User.objects.create_user(username='obama', password='somepassword')
 
     def navbar_test(self,soup):
         navbar = soup.nav
@@ -47,11 +51,13 @@ class TestView(TestCase):
         # 게시물이 2개 있다면
         post_001 = Post.objects.create(
             title='첫번쨰 포스트',
-            content="hello world we are the world"
+            content="hello world we are the world",
+            author=self.user_trump
         )
         post_002 = Post.objects.create(
             title='두번쨰 포스트',
-            content="1등이 전부는 아니잖아요?"
+            content="1등이 전부는 아니잖아요?",
+            author=self.user_obama
         )
         self.assertEqual(Post.objects.count(), 2)
         # 포스트 목록 페이지를 새로고침했을때
@@ -64,11 +70,16 @@ class TestView(TestCase):
         self.assertIn(post_002.title, main_area.text)
         # 아직 게시물이 없습니다 라는 문구는 더이상 보이지 않는다.
         self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
+        self.assertIn(self.user_trump.username.upper(), main_area.text)
+        self.assertIn(self.user_obama.username.upper(), main_area.text)
+
+
 
     def test_post_detail(self):
         post_001 = Post.objects.create(
             title="첫번째 포스트입니다",
-            content='hello world we are the world'
+            content='hello world we are the world',
+            author=self.user_trump,
         )
 
         self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
@@ -82,4 +93,5 @@ class TestView(TestCase):
 
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
+        self.assertIn(self.user_trump.username.upper(), post_area.text)
         self.assertIn(post_001.content, post_area.text)
