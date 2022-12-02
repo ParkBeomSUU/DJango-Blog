38c65be813e9f817832e7c773ff41ebcd88c7d98
BeomSu Park <pps9506@naver.com>
2022-12-02 12:47:40 +0900

    카테고리 페이지 만들기

--- blog/tests.py
+++ blog/tests.py
@@ -32,6 +32,25 @@ class TestView(TestCase) :
             content='카테고리가 없을 수도~',
             author=self.user_obama
         )
+
+
+        def test_category_page(self):
+            response = self.client.get(self.category_programming.get_absolute_url())
+            self.assertEqual(response.status_code, 200)
+
+            soup = BeautifulSoup(response.content,'html.parser')
+            self.navbar_test(soup)
+            self.category_card_test(soup)
+
+            self.assertIn(self.category_programming.name, soup.h1.text)
+
+            main_area = soup.find('div', id='main-area')
+            self.assertIn(self.category_programming.name, main_area.text)
+            self.assertIn(self.post_001.title, main_area.text)
+            self.assertNotIn(self.post_002.title, main_area.text)
+            self.assertNotIn(self.post_003.title, main_area.text)
+
+
     def category_card_test(self, soup) :
         categories_card = soup.find('div', id='categories-card')
         self.assertIn('Categories', categories_card.text)
@@ -55,24 +74,21 @@ class TestView(TestCase) :
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
+
         self.navbar_test(soup)
         self.category_card_test(soup)
 
         main_area = soup.find('div', id='main-area')
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
-        self.assertNotIn('아직 게시물이 없습니다', main_area.text)
 
-        post_001_card = main_area.find('div', id='pos-1')
         post_001_card = main_area.find('div', id='post-1')
         self.assertIn(self.post_001.title, post_001_card.text)
         self.assertIn(self.post_001.category.name, post_001_card.text)
 
-        post_002_card = main_area.find('div', id='pos-2')
         post_002_card = main_area.find('div', id='post-2')
         self.assertIn(self.post_002.title, post_002_card.text)
         self.assertIn(self.post_002.category.name, post_002_card.text)
 
-        post_003_card = main_area.find('div', id='pos-3')
         post_003_card = main_area.find('div', id='post-3')
         self.assertIn('미분류', post_003_card.text)
         self.assertIn(self.post_003.title, post_003_card.text)
@@ -85,20 +101,28 @@ class TestView(TestCase) :
         soup = BeautifulSoup(response.content, 'html.parser')
         main_area = soup.find('div', id='main-area')
         self.assertIn('아직 게시물이 없습니다', main_area.text)
+
+
     def test_post_detail(self):
-        post_001 = Post.objects.create(
-            title='첫 번째 포스트입니다.',
-            content='TDD... 맘에 안 드는 단어야',
-            author=self.user_trump
-        )
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
-        response = self.client.get(post_001.get_absolute_url())
+
+
+        self.assertEqual(self.post_001.get_absolute_url(), '/blog/1/')
+
+        response = self.client.get(self.post_001.get_absolute_url())
+
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
+
         self.navbar_test(soup)
-        self.assertIn(post_001.title, soup.title.text)
+        self.category_card_test(soup)
+
+        self.assertIn(self.post_001.title, soup.title.text)
+
+
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(self.post_001.title, post_area.text)
+        self.assertIn(self.category_programming.name, post_area.text)
+
         self.assertIn(self.user_trump.username.upper(), post_area.text)
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(self.post_001.content, post_area.text)
